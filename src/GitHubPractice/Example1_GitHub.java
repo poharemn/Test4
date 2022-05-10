52933f2e0016fa703a937ce7c153d5b95ada4f3f
Nirmala  Chede <nirmala.chede@dyptc.edu.in>
2022-05-10 18:46:39 +0530

    project upload

---
+++ src/GitHubPractice/Example1_GitHub.java
@@ -0,0 +1,11 @@
+package GitHubPractice;
+
+public class Example1_GitHub 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("First example for GitHub practice");
+		
+	}
+
+}
---
+++ src/GitHubPractice/Example2_GitHub.java
@@ -0,0 +1,11 @@
+package GitHubPractice;
+
+public class Example2_GitHub 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Second example for GitHub practice");
+		
+	}
+
+}
